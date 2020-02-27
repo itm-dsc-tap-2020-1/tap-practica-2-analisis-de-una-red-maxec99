426cb64b3407eb20601ca2c3af2b4e8180296180
maxec99 <61322271+maxec99@users.noreply.github.com>
2020-02-27 12:00:34 -0600

    Add files via upload

---
+++ practica2.py
@@ -0,0 +1,29 @@
+import os
+red="200.33.171.0/24"
+os.system("nmap -sP "+red)
+"""Starting Nmap 7.60 ( https://nmap.org ) at 2020-02-27 11:52 CST
+Nmap scan report for mail.itmorelia.edu.mx (200.33.171.3)
+Host is up (0.012s latency).
+Nmap scan report for 200.33.171.13
+Host is up (0.072s latency).
+Nmap scan report for 200.33.171.63
+Host is up (0.0055s latency).
+Nmap scan report for 200.33.171.66
+Host is up (0.019s latency).
+Nmap scan report for denebvirtual.itmorelia.edu.mx (200.33.171.77)
+Host is up (0.028s latency).
+Nmap scan report for sappp.itmorelia.edu.mx (200.33.171.80)
+Host is up (0.027s latency).
+Nmap scan report for 200.33.171.85
+Host is up (0.032s latency).
+Nmap scan report for 200.33.171.99
+Host is up (0.015s latency).
+Nmap scan report for 200.33.171.115
+Host is up (0.0089s latency).
+Nmap scan report for 200.33.171.120
+Host is up (0.030s latency).
+Nmap scan report for 200.33.171.125
+Host is up (0.028s latency).
+Nmap scan report for 200.33.171.127
+Host is up (0.028s latency).
+"""
